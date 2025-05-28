48d9583aa3189c8800c3c477f781be85f0a82a03
prem056627 <prem@zelthy.com>
2025-05-28 06:39:56 +0530

    fix:mock

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
-// // import React from 'react';
-// // import ReactDOM from 'react-dom/client';
-// // import './index.css';
-// // import App from './App';
-
-// // const root = ReactDOM.createRoot(document.getElementById('root'));
-// // root.render(<App />);
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// // reportWebVitals();
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// // import './i18n.js'
-// import { Suspense } from 'react';
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import App from './App'
 
 // async function enableMocking() {
-// 	if (process.env.REACT_APP_MSW_MOCK_API === 'true') {
-// 		const { worker } = await import('./mocks/browser');
-// 		return worker.start();
-// 	} else {
-// 		return;
-// 	}
+//   if (process.env.NODE_ENV === 'development') {
+//     const { worker } = await import('./mocks/browser')
+//     return worker.start({
+//       onUnhandledRequest: 'bypass'
+//     })
+//   }
 // }
 
 // enableMocking().then(() => {
-// 	const root = ReactDOM.createRoot(document.getElementById('root'));
-// 	root.render(<App />);
-// });
-
+//   const root = ReactDOM.createRoot(document.getElementById('root'))
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   )
+// })
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -41,7 +27,7 @@ import './index.css'
 import App from './App'
 
 async function enableMocking() {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' || process.env.REACT_APP_USE_MOCKS === 'true') {
     const { worker } = await import('./mocks/browser')
     return worker.start({
       onUnhandledRequest: 'bypass'
